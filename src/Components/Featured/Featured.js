81a6d2efa2509a0c4478b51384aa674ec3e4f4b4
haneeenedinat <haneen.aledinat@gmail.com>
2022-01-22 21:15:30 +0200

    featured

--- src/Components/Featured/Featured.js
+++ src/Components/Featured/Featured.js
@@ -1,23 +1,24 @@
 import React from "react";
+import "./Featured.css";
 
-export default function Featured(){
-  return(
-      <div className="OurFeatured">
-
-          <div className="Featuredh2">
-              <h2>Our Featured</h2>
-          </div>
-
-          <div className="Featured">
-           <div className="FeaturedBeneficiaries">
-           The number of volunteers is 100 volunteers from all governorates of the Kingdom
-           </div>
-
-           <div className="FeaturedVolunteers">
-           The number of beneficiaries 150 families from all governorates of the Kingdom
-           </div>
-           </div>
+export default function Featured() {
+  return (
+    <div className="OurFeatured">
+      <div className="Featuredh2">
+        <h2>Our Featured</h2>
       </div>
-  )
-}
 
+      <div className="Featured">
+        <div className="FeaturedBeneficiaries">
+          The number of volunteers is 100 volunteers from all governorates of
+          the Kingdom
+        </div>
+
+        <div className="FeaturedVolunteers">
+          The number of beneficiaries 150 families from all governorates of the
+          Kingdom
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Background from "../Components/Background/Background";
 import WeProvide from "../Components/WeProvide/WeProvide";
+import Featured from "../Components/Featured/Featured";
+
 
 export default function HomePage(){
     return(
         <div>
             <Background/>
             <WeProvide/>
+            <Featured/>
         </div>
     )
 }
